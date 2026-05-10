879a85083ec41021fe701b3634c91ddb290d9262
jhonatan-mm <jhonatan.mamani.m9@gmail.com>
2026-05-09 23:09:36 -0400

    implementacion de copia de seguridad y portafolio

--- src/services/backups.service.ts
+++ src/services/backups.service.ts
@@ -10,7 +10,7 @@ const handleUnauthorized = () => {
   window.location.href = "/login";
 };
 
-/**prueba
+/**PRUEBA
  * Genera un backup de la base de datos y lo descarga automáticamente.
  * El backend devuelve el archivo SQL directamente.
  */
